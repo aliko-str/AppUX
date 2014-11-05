3a89fe9e8b41c3aed7f476202648ce8cbaca51a5
Aliaksei <a.minyukovich@gmail.com>
2014-11-05 19:28:33 +0100

    scenario module

---
+++ data/module.scenario/scenario.client.js
@@ -0,0 +1,15 @@
+(function() {
+	var ap = window.AppUX;
+	var imgToLoad = {
+	};
+	var cssToLoad = ["side.libs/bootstrap.min.css", "module.scenario/main.css"];
+	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++){
+		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function(){});
+	}
+	for(var i = 0, ilen = cssToLoad.length; i < ilen; i++){
+		ap.loadCss(ap.baseUrl + cssToLoad[i]);
+	}
+	window.document.getElementById("finishReading").onclick = function(ev){
+		ap.port.emit("next", "let's roll!");
+	};
+})();
