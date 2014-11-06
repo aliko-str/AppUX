ffa76e872f9b8b46a70c3f1e2c64aecabafb9952
Aliaksei <a.minyukovich@gmail.com>
2014-11-06 11:42:10 +0100

    website rating module + next step module; Not debugged

---
+++ data/module.next.step.message/next.step.client.js
@@ -0,0 +1,20 @@
+//nextStepText
+
+(function() {
+	var ap = window.AppUX;
+	var imgToLoad = {
+	};
+	var cssToLoad = ["side.libs/bootstrap.min.css", "module.scenario/main.css"];
+	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++){
+		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function(){});
+	}
+	for(var i = 0, ilen = cssToLoad.length; i < ilen; i++){
+		ap.loadCss(ap.baseUrl + cssToLoad[i]);
+	}
+	window.document.getElementById("finishReading").onclick = function(ev){
+		ap.port.emit("next", "let's roll!");
+	};
+	ap.port.on("nextStepText", function(nextStepText){
+		window.document.getElementById("nextStepText").textContent = nextStepText;
+	});
+})();
