5b9a237d9e7b1dbe1eeace521194b19d53d839c1
Aliaksei <a.minyukovich@gmail.com>
2014-11-06 19:18:34 +0100

    fix for loading any content in an iframe (via response header rewriting)

--- data/module.website.ratings/website.rating.client.js
+++ data/module.website.ratings/website.rating.client.js
@@ -1,60 +1,72 @@
 (function() {
+	"use strict";
 	var ap = window.AppUX;
 	var imgToLoad = {
 	};
 	var websitesToRate = ap.options.websitesToRate;
-	if(ap.options.debug){
+	if(ap.options.debug) {
 		//fool check
-		if(!websitesToRate instanceof Array){
+		if(!websitesToRate instanceof Array) {
 			throw new Error("#f0fm7 websitesToRate wasn't an array");
 		}
-		if(!ap.options.question){
+		if(!websitesToRate.length) {
+			throw new Error("this module makes no sense without websites to rate -- the input array of websites is empty");
+		}
+		if(!ap.options.question) {
 			throw new Error("there shall be a question to ask!");
 		}
-		if(!ap.options.apUXWebsMinLabel || !ap.options.apUXWebsMaxLabel){
+		if(!ap.options.apUXWebsMinLabel || !ap.options.apUXWebsMaxLabel) {
 			throw new Error("max and min labels are missing for a question");
 		}
 	}
 	websitesToRate = websitesToRate.shuffle();
 	var cssToLoad = ["side.libs/bootstrap.min.css", "module.website.ratings/main.css"];
-	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++){
-		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function(){});
+	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++) {
+		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function() {
+		});
 	}
-	for(var i = 0, ilen = cssToLoad.length; i < ilen; i++){
+	for(var i = 0, ilen = cssToLoad.length; i < ilen; i++) {
 		ap.loadCss(ap.baseUrl + cssToLoad[i]);
 	}
-	
-	// window.document.getElementById("finishReading").onclick = function(ev){
-		// ap.port.emit("next", "let's roll!");
-	// };
+
 	$("#apUXWebsQuestion").text(ap.options.question);
 	$("#apUXWebsMinLabel").text(ap.options.apUXWebsMinLabel);
 	$("#apUXWebsMaxLabel").text(ap.options.apUXWebsMaxLabel);
-	$("form").submit(function(ev){
+	setWebsiteToRate(websitesToRate.shift());
+	var ratingArr = [];
+	$("form").submit(function(ev) {
 		$(this).find("input[type='button']").val("Saving it...").prop("disabled", true);
-		window.setTimeout(function(){
-			setWebsiteToRate(websitesToRate.shift());
+		var data = {};
+		$(this).serializeArray().forEach(function(el, i) {
+			data[el.name] = el.value;
+		});
+		ratingArr.push(data);
+		window.setTimeout(function() {
+			if(websitesToRate.length) {
+				setWebsiteToRate(websitesToRate.shift());
+			} else {
+				saveWebsiteRatings(ratingArr);
+			}
 			return;
 		}, 500);
 		ev.preventDefault();
 	});
 
-	function setWebsiteToRate(website){
-		if(website){
-			$("input[type='button']").val("Rate the website").prop("disabled", false);
-			$("#apUXWebsHostname").val(website.hostname);
-			$("#apUXWebsPreviewRoot").attr("srt", website.url);
-		}else{
-			var dataRaw = $(this).serializeArray();
-			var data = {};
-			$.each(dataRaw, function(i, field) {
-				data[field.name] = field.value;
-			});
-			ap.port.emit("data", data);
-			setTimeout(function(){
-				ap.port.emit("next", "let's roll!");
-			}, 500);
-		}
-		return (website && true);
+	function saveWebsiteRatings(ratingArr) {
+		ap.port.emit("data", ratingArr);
+		ap.port.emit("next", "let's roll!");
+		return ratingArr.length;
+	}
+
+	function setWebsiteToRate(website) {
+		$("form").trigger("reset");
+		$("input[type='button']").val("Rate the website").prop("disabled", false);
+		$("#apUXWebsHostname").val(website.hostname);
+		$("#apUXWebsPreviewRoot").attr("src", website.url);
+		//$("input[type='radio']").prop("checked", false);
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+		return true;
 	};
 })();
