d49acc75b93a1e7eb7f4365d99b898911d562bf2
Aliaksei <a.minyukovich@gmail.com>
2014-11-07 16:57:22 +0100

    screenshot 4s rating module

---
+++ data/module.screenshot.4s.ratings/screenshot.4s.ratings.client.js
@@ -0,0 +1,135 @@
+(function() {
+	"use strict";
+	var ap = window.AppUX;
+	var imgToLoad = {
+	};
+	var screenshotsToRate = ap.options.screenshotsToRate;
+	if(ap.options.debug) {
+		//fool check
+		if(!screenshotsToRate instanceof Array) {
+			throw new Error("#f0fm7 screenshotsToRate wasn't an array");
+		}
+		if(!screenshotsToRate.length) {
+			throw new Error("this module makes no sense without screenshots to rate -- the input array of screenshots is empty");
+		}
+		if(!ap.options.question) {
+			throw new Error("there shall be a question to ask!");
+		}
+		if(!ap.options.apUXWebsMinLabel || !ap.options.apUXWebsMaxLabel) {
+			throw new Error("max and min labels are missing for a question");
+		}
+	}
+	screenshotsToRate = screenshotsToRate.shuffle();
+	var cssToLoad = ["side.libs/bootstrap.min.css", "module.screenshot.4s.ratings/main.css"];
+	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++) {
+		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function() {
+		});
+	}
+	for(var i = 0, ilen = cssToLoad.length; i < ilen; i++) {
+		ap.loadCss(ap.baseUrl + cssToLoad[i]);
+	}
+
+	(function setUpVisuals(screenList) {
+		var timestampFrom;
+		var timestampTo;
+		var dataArr = [];
+		$("#apUXWebsQuestion").text(ap.options.question);
+		$("#apUXWebsMinLabel").text(ap.options.apUXWebsMinLabel);
+		$("#apUXWebsMaxLabel").text(ap.options.apUXWebsMaxLabel);
+		setScreenshotToRate(ap, screenList.shift(), function() {
+			timestampFrom = Date.now();
+		});
+		$("form").submit(function(ev) {
+			var jqForm = $(this);
+			jqForm.find("input[type='button']").val("Saving it...").prop("disabled", true);
+			var data = {
+				fname : jqForm.find("input[name='fname']").val(),
+				time : timestampTo - timestampFrom,
+				score : jqForm.find("input[name='apUXWebsRating']:checked").val()
+			};
+			dataArr.push(data);
+			window.setTimeout(function() {
+				if(screenList.length) {
+					setScreenshotToRate(ap, screenList.shift(), function() {
+						timestampFrom = Date.now();
+					});
+				} else {
+					saveScreenshotRatingsTimeToRate(dataArr);
+				}
+				return;
+			}, 500);
+			ev.preventDefault();
+		});
+		$("input[type='radio']").click(function(ev) {
+			timestampTo = Date.now();
+		});
+	})(screenshotsToRate.shuffle());
+
+	function saveScreenshotRatingsTimeToRate(dataArr) {
+		ap.port.emit("data", dataArr);
+		ap.port.emit("next", "let's roll!");
+		return dataArr.length;
+	}
+
+	function disableSubmitForNSec(jqSubmit, sec, clickCallback, unfreezeCallback) {
+		var delaySec = sec || 5;
+		var strVal = "Unfreezes in ";
+		var oldStrVal = jqSubmit.val();
+		jqSubmit.attr("title", "You can't submit answers before the button unfreezes. This is a part of our anti-bot policy.");
+		jqSubmit.attr("style", "box-shadow: none;cursor: not-allowed;opacity: 0.5;");
+		jqSubmit.tooltip();
+		jqSubmit.hover(function(ev) {
+			ev.preventDefault();
+		}, function(ev) {
+			ev.preventDefault();
+		});
+		jqSubmit.click(function(ev) {
+			ev.preventDefault();
+		});
+		function tick(secondLeft) {
+			if(secondLeft) {
+				jqSubmit.val(strVal + secondLeft.toString());
+				window.setTimeout(function() {
+					tick(secondLeft - 1);
+				}, 1000);
+			} else {
+				jqSubmit.val(oldStrVal);
+				jqSubmit.removeAttr("style");
+				jqSubmit.off("click");
+				jqSubmit.off("hover");
+				if(clickCallback) {
+					jqSubmit.click(clickCallback);
+				}
+				jqSubmit.removeAttr("title");
+				jqSubmit.removeAttr("data-original-title");
+				jqSubmit.tooltip({
+					disabled : true
+				});
+				unfreezeCallback();
+			}
+		};
+		tick(delaySec);
+	};
+
+	function setScreenshotToRate(ap, screenshot, unfreezeCallback) {
+		var jqForm = $("form");
+		jqForm.trigger("reset");
+		jqForm.find("input[name='fname']").val(screenshot.fname);
+		ap.loadImage("apUXScreenshotRoot", screenshot.url, function() {
+			var jqSbm = jqForm.find("input[type='submit']").val("Rate the webpage");
+			jqSbm.prop("disabled", false);
+			disableSubmitForNSec(jqSbm, 4, function() {
+				//no-op
+			}, unfreezeCallback);
+		});
+		var tmpIm = new Image();
+		tmpIm.onload = function() {
+			$("#apUXScreenshotRoot").height(tmpIm.height);
+		};
+		tmpIm.src = screenshot.url;
+		$('html, body').animate({
+			scrollTop : 0
+		}, 0);
+		return;
+	};
+})();
--- data/module.website.ratings/website.rating.client.js
+++ data/module.website.ratings/website.rating.client.js
@@ -19,7 +19,6 @@
 			throw new Error("max and min labels are missing for a question");
 		}
 	}
-	websitesToRate = websitesToRate.shuffle();
 	var cssToLoad = ["side.libs/bootstrap.min.css", "module.website.ratings/main.css"];
 	for(var i = 0, _keys = Object.keys(imgToLoad), ilen = _keys.length; i < ilen; i++) {
 		ap.loadImage(_keys[i], ap.baseUrl + imgToLoad[_keys[i]], function() {
@@ -29,44 +28,61 @@
 		ap.loadCss(ap.baseUrl + cssToLoad[i]);
 	}
 
-	$("#apUXWebsQuestion").text(ap.options.question);
-	$("#apUXWebsMinLabel").text(ap.options.apUXWebsMinLabel);
-	$("#apUXWebsMaxLabel").text(ap.options.apUXWebsMaxLabel);
-	setWebsiteToRate(websitesToRate.shift());
-	var ratingArr = [];
-	$("form").submit(function(ev) {
-		$(this).find("input[type='button']").val("Saving it...").prop("disabled", true);
-		var data = {};
-		$(this).serializeArray().forEach(function(el, i) {
-			data[el.name] = el.value;
+	(function setUpVisuals(websList) {
+		var timestampFrom;
+		var timestampTo;
+		var timestampOnLoad;
+		var dataArr = [];
+		$("#apUXWebsQuestion").text(ap.options.question);
+		$("#apUXWebsMinLabel").text(ap.options.apUXWebsMinLabel);
+		$("#apUXWebsMaxLabel").text(ap.options.apUXWebsMaxLabel);
+		timestampFrom = setWebsiteToRate(websList.shift(), function() {
+			timestampOnLoad = Date.now();
 		});
-		ratingArr.push(data);
-		window.setTimeout(function() {
-			if(websitesToRate.length) {
-				setWebsiteToRate(websitesToRate.shift());
-			} else {
-				saveWebsiteRatings(ratingArr);
-			}
-			return;
-		}, 500);
-		ev.preventDefault();
-	});
+		$("form").submit(function(ev) {
+			var jqForm = $(this);
+			jqForm.find("input[type='button']").val("Saving it...").prop("disabled", true);
+			var data = {
+				webRoot : jqForm.find("input[name='webRoot']").val(),
+				fullTime : timestampTo - timestampFrom,
+				onLoadTime : timestampTo - timestampOnLoad,
+				score : jqForm.find("input[name='apUXWebsRating']:checked").val()
+			};
+			dataArr.push(data);
+			window.setTimeout(function() {
+				if(websList.length) {
+					timestampFrom = setWebsiteToRate(websList.shift(), function() {
+						timestampOnLoad = Date.now();
+					});
+				} else {
+					saveWebsiteRatingsTimeToRate(dataArr);
+				}
+				return;
+			}, 500);
+			ev.preventDefault();
+		});
+		$("input[type='radio']").click(function(ev) {
+			timestampTo = Date.now();
+		});
+	})(websitesToRate.shuffle());
 
-	function saveWebsiteRatings(ratingArr) {
-		ap.port.emit("data", ratingArr);
+	function saveWebsiteRatingsTimeToRate(dataArr) {
+		ap.port.emit("data", dataArr);
 		ap.port.emit("next", "let's roll!");
-		return ratingArr.length;
+		return dataArr.length;
 	}
 
-	function setWebsiteToRate(website) {
-		$("form").trigger("reset");
-		$("input[type='button']").val("Rate the website").prop("disabled", false);
-		$("#apUXWebsHostname").val(website.hostname);
-		$("#apUXWebsPreviewRoot").attr("src", website.url);
+	function setWebsiteToRate(website, onLoadCallback) {
+		var jqForm = $("form");
+		jqForm.trigger("reset");
+		jqForm.find("input[type='button']").val("Rate the website").prop("disabled", false);
+		jqForm.find("input[name='webRoot']").val(website.hostname);
+		// $("#apUXWebsHostname").val(website.hostname);
+		$("#apUXWebsPreviewRoot").attr("src", website.url).load(onLoadCallback);
 		//$("input[type='radio']").prop("checked", false);
-    $('html, body').animate({
-        scrollTop: 0
-    }, 1000);
-		return true;
+		$('html, body').animate({
+			scrollTop : 0
+		}, 700);
+		return Date.now();
 	};
 })();
